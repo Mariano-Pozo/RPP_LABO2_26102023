81c76d23ad19efd59fdd45bf29f6c1c674f87b27
Mariano-Pozo <mlxito.nax2@gmail.com>
2023-10-26 19:17:03 -0300

    primer comit class vehiculo

---
+++ Entidades/Vehiculo.cs
@@ -0,0 +1,40 @@
+﻿namespace Entidades
+{
+    public abstract class Vehiculo
+    {
+        protected Vehiculo(EPropulsion propulsion)
+        {
+            
+        }
+        protected Vehiculo(EPropulsion propulsion, bool esAWD)
+        {
+
+        }
+
+        protected bool esAWD;
+        protected Guid numeroDeChasis;
+        protected EPropulsion propulsion;
+
+        //propiedades
+        public EPropulsion Propulsion 
+        { 
+            get;
+        }
+        public abstract string Tipo 
+        {  
+            get;
+        }
+
+        protected string GetInfo()
+        {
+            return "";
+        }
+
+        public override string ToString()
+        {
+            return Tipo; //?
+        }
+
+        
+    }
+}
