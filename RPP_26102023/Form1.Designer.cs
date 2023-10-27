5cd825ecb384cf050082a568459f91312cfc0639
Mariano-Pozo <mlxito.nax2@gmail.com>
2023-10-26 21:06:26 -0300

    form 1

--- RPP_26102023/Form1.Designer.cs
+++ RPP_26102023/Form1.Designer.cs
@@ -28,8 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             button1 = new Button();
             button2 = new Button();
+            pictureBox2 = new PictureBox();
+            comboBox1 = new ComboBox();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // button1
@@ -38,7 +43,7 @@
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 0;
-            button1.Text = "button1";
+            button1.Text = "Fabricar";
             button1.UseVisualStyleBackColor = true;
             // 
             // button2
@@ -47,23 +52,58 @@
             button2.Name = "button2";
             button2.Size = new Size(75, 23);
             button2.TabIndex = 1;
-            button2.Text = "button2";
+            button2.Text = "Eliminar";
             button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(649, 182);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(75, 78);
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Automovil", "Camioneta", "Moto" });
+            comboBox1.Location = new Point(649, 42);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(649, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(97, 15);
+            label1.TabIndex = 5;
+            label1.Text = "Tipo de Vehiculo:";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(comboBox1);
+            Controls.Add(pictureBox2);
             Controls.Add(button2);
             Controls.Add(button1);
             Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
         private Button button2;
+        private PictureBox pictureBox2;
+        private ComboBox comboBox1;
+        private Label label1;
     }
 }
